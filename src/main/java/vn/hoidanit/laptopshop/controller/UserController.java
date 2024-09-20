b058a22364b7ce1ad3c1321da79d7ed7f01a11d4
B23DCAT011 <ducanhluust@gmail.com>
2024-09-20 12:17:22 +0700

    done

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -2,12 +2,11 @@ package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RequestMethod;
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -24,6 +23,19 @@ public class UserController {
     model.addAttribute("hoidanit", "hello from controller");
     return "hello";
   }
+
+  @RequestMapping("/admin/user")
+  public String getUserPage(Model model) {
+    model.addAttribute("newUser", new User());
+    return "admin/user/create";
+  }
+
+  @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+  public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+    System.out.println("RunHere" + hoidanit);
+    return "hello";
+  }
+
 }
 
 // }
